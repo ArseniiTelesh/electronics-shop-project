9b5441ba56684c1997c1ede8b9b229d33610abbb
Arsenii Telesh <telesh.sen98@gmail.com>
2023-05-17 19:35:59 +0600

    Made homework_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def screen():
+    return Item("Монитор", 5000, 10)
+
+
+def test_calculate_total_price(screen):
+    assert screen.calculate_total_price() == 50000
+
+
+def test_apply_discount(screen):
+    Item.pay_rate = 0.5
+    assert screen.apply_discount() is None
+    assert screen.price == 2500
